d26563256f52f6227c9447f974a2ecd2061cb5f4
Brian Duong <brianduongh@gmail.com>
2018-12-06 23:37:57 -0600

    added seeds/schema

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+// mySQL connection
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 8889,
+  user: 'root',
+  password: 'root',
+  database: 'burgers_db'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+  console.log('connected as id : ' connection.threadId);
+});
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Seeds for db
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("veggieburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("smashburger", false);
